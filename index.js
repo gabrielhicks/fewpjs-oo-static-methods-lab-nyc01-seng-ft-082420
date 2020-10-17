a89c77cd57df16d5c25e39e22751acd9eefd67fe
gabrielhicks <gabrielhicks@mail.com>
2020-10-17 15:00:40 -0400

    Okay

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+  }
+
+  static titleize(string) {
+
+  }
 }
